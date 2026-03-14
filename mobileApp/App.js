baded52544a7d3fbf14f5feb3313dead5f7a5e4d
GermaniumMo <morinagermanium@gmail.com>
2026-03-14 12:50:21 +0100

    add

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -3,14 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/MainScreen';
+import ExerciseScreen from './screens/ExerciseScreen';
+import ListScreen from './screens/ListScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName='List'
+        screenOptions={{
+          headerTitle: 'APP',
+        }}
+      >
+        <Stack.Screen name = "Exercise" component={ExerciseScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name='List' component={ListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ mobileApp/screens/ListScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {Text, StyleSheet, View, FlatList} from 'react-native';
+
+const students = [
+    {name: 'Germanium', surname: 'Morina', age: 30},
+    {name: 'Elvedin', surname: 'Blakqori', age: 18},
+    {name: 'Yll', surname: 'Gashi', age: 16},
+    {name: 'Enes', surname: 'Mulolli', age: 18},
+    {name: 'Drilon', surname: 'Latifi', age: 18},
+    {name: 'Yll', surname: 'Berisha', age: 18}
+]
+
+const ListScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textStyle}>List of Students</Text>
+            <FlatList
+                data={students}
+                horizontal={true}
+                showsHorizontalScrollIndicat = {false}
+                keyExtractor={student => student.name}
+                renderItem={({item}) => {
+                    return <Text style={styles.studentWrapper}>{item.name} {item.surname} - {item.age}</Text>
+                }}
+            />
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#fff"
+    },
+    textStyle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    studentWrapper: {
+        fontSize: 20,
+        marginRight: 20,
+        padding: 10,
+        backgroundColor: "#f0f0f0",
+        borderRadius: 5,
+        widht: 300,
+    }
+});
+
+export default ListScreen;
