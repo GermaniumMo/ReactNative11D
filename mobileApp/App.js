5c40ccf7aa554202d5eccb0e2a060e4239d96c43
GermaniumMo <morinagermanium@gmail.com>
2026-03-28 12:51:01 +0100

    add

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -5,21 +5,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/MainScreen';
 import ExerciseScreen from './screens/ExerciseScreen';
 import ListScreen from './screens/ListScreen';
-
+import ButtonScreen from './screens/ButtonScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='List'
+        initialRouteName='Button'
         screenOptions={{
           headerTitle: 'APP',
         }}
       >
+        <Stack.Screen name = "Button" component={ButtonScreen} />
         <Stack.Screen name = "Exercise" component={ExerciseScreen} />
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name='List' component={ListScreen} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
